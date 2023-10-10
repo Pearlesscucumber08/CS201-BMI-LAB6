8a6d6d2454d5f04a166cf1956f1afac4630426af
Brigitte Mae Ibeng <115964613+Pearlesscucumber08@users.noreply.github.com>
2023-10-10 14:58:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstName = " Brigitte Mae";
+let last_name = " Ibeng";
+let fullName = firstName + ' ' + last_name;
+console.log (fullName);
+console.log("BSCS-2A");
+console.log("CS201-BMI-LAB6");
+
+let yourName = 10;
+let name = prompt (" Enter your name ");
+while (yourName <101) {
+    console.log (name, yourName);
+    yourName++;
+}
